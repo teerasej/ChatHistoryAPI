ace78a842bb7f05f3e876dfce703fd4178a7c20a
Teerasej <teerasej@gmail.com>
2024-05-10 19:06:01 +0700

    add validation rule to each field

--- Models/Topic.cs
+++ Models/Topic.cs
@@ -7,6 +7,9 @@ namespace ChatHistoryAPI.Models
     {
         [Key]
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "ไม่ใส่ชื่อหัวข้อได้ไง")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "ชื่อไม่ควรน้อยกว่า 3 และไม่มากกว่า 100 ตัวอักษร")]
         public string Name { get; set; }
         public string UserID { get; set; }
         public ICollection<Message> Messages { get; set; } = new List<Message>();
